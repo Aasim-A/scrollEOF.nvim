345deec622318b70f35c0ff06debf4217c9da98d
Steven Xu <stevenxxiu@users.noreply.github.com>
2024-01-08 12:02:55 +1100

    refactor: use screen line APIs to work with wrapped lines, fixes #3 (#12)
    * refactor: use screen line APIs to work with wrapped lines, fixes #3
    
    * Removed unnecessary code
    
    ---------
    
    Co-authored-by: Aasim-A <23695024+Aasim-A@users.noreply.github.com>

--- lua/scrollEOF.lua
+++ lua/scrollEOF.lua
@@ -3,50 +3,19 @@ local M = {}
 local mode_disabled = false
 local filetype_disabled = false
 
----Counts the number of folded lines between two line numbers
----@param lnum1 number
----@param lnum2 number
----@return number
-local function folded_lines_between(lnum1, lnum2)
-  local next_fold_end_ln = -1
-  local folded_lines = 0
-
-  for ln = lnum1, lnum2, 1 do
-    if ln > next_fold_end_ln then -- skip folded lines we already added to the count
-      next_fold_end_ln = vim.fn.foldclosedend(ln)
-      local is_folded_line = next_fold_end_ln ~= -1
-      if is_folded_line then
-        local fold_size = next_fold_end_ln - ln
-        folded_lines = folded_lines + fold_size
-      end
-    end
-  end
-
-  return folded_lines
-end
-
 local function check_eof_scrolloff()
   if mode_disabled or filetype_disabled then
     return
   end
 
   local win_height = vim.fn.winheight(0)
-  local last_line = vim.fn.line('$')
-  local win_last_line = vim.fn.line('w$')
-
-  -- PERF avoid calculations when far away from the end of file
-  if win_last_line + win_height < last_line then return end
-
-  local win_view = vim.fn.winsaveview()
+  local win_cur_line = vim.fn.winline()
   local scrolloff = math.min(vim.o.scrolloff, math.floor(win_height / 2))
-  local cur_line = win_view.lnum
-  local win_top_line = win_view.topline
-  local visual_distance_to_eof = last_line - cur_line - folded_lines_between(cur_line, last_line)
-  local visual_last_line_in_win = win_last_line - folded_lines_between(win_top_line, win_last_line)
-  local scrolloff_line_count = win_height - (visual_last_line_in_win - win_top_line + 1)
+  local visual_distance_to_eof = win_height - win_cur_line
 
-  if visual_distance_to_eof < scrolloff and scrolloff_line_count + visual_distance_to_eof < scrolloff then
-    vim.fn.winrestview({ topline = win_view.topline + scrolloff - (scrolloff_line_count + visual_distance_to_eof) })
+  if visual_distance_to_eof < scrolloff then
+    local win_view = vim.fn.winsaveview()
+    vim.fn.winrestview({ topline = win_view.topline + scrolloff - visual_distance_to_eof })
   end
 end
 
